42520ff9e7f476b340688413fae5e805a9c6d577
alexanderyoza <alex.yoza@gmail.com>
2022-10-15 21:42:18 -0600

    workoutplan

--- react/src/Components/WorkoutPlan.js
+++ react/src/Components/WorkoutPlan.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function WorkoutPlan(props) {
 
 
-    const [plan, setPlan] = useState([<div>hello</div>]);
+    const [plan, setPlan] = useState([]);
+
+    useEffect(() => {
+        workouts();
+    })
 
     //called to create a single day to return to workouts
-    const workday = () => {
-        return <div>Day</div>;
+    const workday = (dayNum) => {
+        return <div>Day {dayNum}</div>;
     }
 
     //loops through the amount of days to create a workout
     const workouts = () => {
-        for (let i = 0; i < props.day; i++){
-            plan.push(workday());
+        for (let i = 0; i < props.days; i++){
+            plan.push(workday(i+1));
         }
     }
 
